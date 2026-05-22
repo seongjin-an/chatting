c7516ff31657a0d16ca928e21e33b9e625cb31a6
seongjin-an <seongjin4us@gmail.com>
2026-05-22 17:17:40 +0900

    feature: user http

--- user-service/src/main/java/com/chat/user/UserServiceApplication.java
+++ user-service/src/main/java/com/chat/user/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.chat.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.chat"})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
